d88ac70c8e7090655d4163cdd663b1b99cabeacc
rosakun <65656375+rosakun@users.noreply.github.com>
2022-04-15 22:09:22 +0200

    Changed baseline.py so it doesn't ask for version

--- baseline.py
+++ baseline.py
@@ -9,8 +9,6 @@ import sys
 
 def summarize(file):
 
-    version = input("Which version would you like to use? [baseline] ")
-
     # Open file and save each sentence to a different line
     with open(file) as f:
         sentences = [sentence.rstrip() for sentence in f.readlines()]
@@ -26,32 +24,31 @@ def summarize(file):
     print('Generator loaded.')
 
     # Query where details of the sentence selection are specified
-    if version == 'baseline':
-        select = input("Choose: random or first?")
-        n = int(input("How many sentences should the summary have?"))
+    select = input("Choose: random or first?")
+    n = int(input("How many sentences should the summary have?"))
 
-        # Selection of sentences
-        if select == 'random':
-            if len(sentences) > int(n):
-                summary_AMRs = random.sample(sentences,k=n)
-            else:
-                summary_AMRs = sentences
+    # Selection of sentences
+    if select == 'random':
+        if len(sentences) > int(n):
+            summary_AMRs = random.sample(sentences,k=n)
+        else:
+            summary_AMRs = sentences
 
-        if select == 'first':
-            if len(sentences) > int(n):
-                summary_AMRs = sentences[:int(n)]
-            else:
-                summary_AMRs = sentences
+    if select == 'first':
+        if len(sentences) > int(n):
+            summary_AMRs = sentences[:int(n)]
+        else:
+            summary_AMRs = sentences
 
-        # Parse the sentences
-        parser_output = stog.parse_sents(summary_AMRs)
+    # Parse the sentences
+    parser_output = stog.parse_sents(summary_AMRs)
 
-        # Generate the sentences
-        sents, _ = gtos.generate(summary_AMRs, disable_progress=False, use_tense=False)
+    # Generate the sentences
+    sents, _ = gtos.generate(summary_AMRs, disable_progress=False, use_tense=False)
 
-        # Write the sentences to an output file
-        with open("summary.txt", "w") as o:
-            o.writelines(sents)
+    # Write the sentences to an output file
+    with open("summary.txt", "w") as o:
+        o.writelines(sents)
 
 
 summarize(sys.argv[1])
